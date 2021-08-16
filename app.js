e11fd0415d894a54cd7d765e555ef5f06f23ece0
Arman Naserani <arman.na.462@gmail.com>
2021-08-16 14:09:16 +0430

    Get searched phrase

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function (req, res) {
+    let searchedPhrase = req.body.search_input;
+    console.log(searchedPhrase);
 })
 
 app.listen(3000, function () {
